ee2937f6fae4666aefb70a35ce6436528d9343ff
Sathy Akter <68398295+Sathy77@users.noreply.github.com>
2021-02-01 00:30:20 +0600

    Add files via upload

---
+++ Full_Adder/Full_Adder.ino
@@ -0,0 +1,20 @@
+int A, B, C;
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(10, INPUT);
+  pinMode(9, INPUT);
+  pinMode(8, INPUT);
+
+}
+
+void loop() {
+  A = digitalRead(10);
+  A = digitalRead(9);
+  A = digitalRead(8);
+  int S, Carry;
+  S = A ^ B ^ C;
+  Carry = ((A & B) | (B & C) | (A & C));
+  digitalWrite(13, S);
+  digitalWrite(12, Carry);
+}
