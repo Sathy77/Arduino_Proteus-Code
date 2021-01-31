6f71b5021667fb5013d0ce7987ccc48b6b3cee2c
Sathy Akter <68398295+Sathy77@users.noreply.github.com>
2021-02-01 00:30:02 +0600

    Add files via upload

---
+++ Half_Adder/Huf_Adder.ino
@@ -0,0 +1,19 @@
+int A,B;
+void setup() {
+pinMode(13,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(11,INPUT);
+pinMode(10,INPUT);
+
+}
+
+void loop() {
+  A = digitalRead(10); 
+  B = digitalRead(11);
+  int s,c;
+
+  s=A^B;
+  c=A&B;
+  digitalWrite(13, s);
+  digitalWrite(12, c);
+}
