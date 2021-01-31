e975cc85717168b891b4803ad4a29459847777df
Sathy Akter <68398295+Sathy77@users.noreply.github.com>
2021-02-01 00:31:24 +0600

    Add files via upload

---
+++ Digital led/work.ino
@@ -0,0 +1,19 @@
+
+//My ID is 18101077
+int Led[7] = {3, 4, 5, 6, 7, 8, 9};
+void setup() {
+  for (int i = 0; i <= 6; i++)
+  {
+    pinMode(Led[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i <= 6; i++)
+  {
+    digitalWrite(Led[i], HIGH);
+    delay(100);
+    digitalWrite(Led[i], LOW);
+    delay(100);
+  }
+}
