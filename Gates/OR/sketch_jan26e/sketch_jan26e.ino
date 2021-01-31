77826d16a6699c901cba22c3d81668f8832a1424
Sathy Akter <68398295+Sathy77@users.noreply.github.com>
2021-02-01 00:29:36 +0600

    Add files via upload

---
+++ Gates/OR/sketch_jan26e/sketch_jan26e.ino
@@ -0,0 +1,22 @@
+//OR Gate
+
+int buttonStatus1, buttonStatus2;
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, INPUT);
+  pinMode(11, INPUT);
+
+}
+
+void loop() {
+  buttonStatus1 = digitalRead(12);
+  buttonStatus2 = digitalRead(11);
+  if(buttonStatus1==HIGH || buttonStatus2==HIGH)
+  {
+    digitalWrite(13,HIGH);
+  }
+  else
+  {
+    digitalWrite(13,LOW);
+  }
+}
---
+++ Gates/XNOR/sketch_jan26f.ino
@@ -0,0 +1,22 @@
+//XNOR Gate
+
+int buttonStatus1, buttonStatus2;
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, INPUT);
+  pinMode(11, INPUT);
+
+}
+
+void loop() {
+  buttonStatus1 = digitalRead(12);
+  buttonStatus2 = digitalRead(11);
+  if((buttonStatus1==HIGH && buttonStatus2==HIGH)||(buttonStatus1==LOW && buttonStatus2==LOW))
+  {
+    digitalWrite(13,HIGH);
+  }
+  else
+  {
+    digitalWrite(13,LOW);
+  }
+}
